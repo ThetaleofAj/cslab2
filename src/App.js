efb51c7845b2183f87e7a27a5cb80a4546659741
unknown <you@example.com>
2022-05-19 10:22:09 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
-import { BrowserRouter,Route,Routes, Switch } from "react-router-dom";
+import { BrowserRouter,Route,Routes} from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 
